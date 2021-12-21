8d5931c4afb261645d896f429182d67f7947b761
Hyago M. Vale <hyagomvale@gmail.com>
2021-12-21 12:04:31 -0300

    Diz "Hello World"

---
+++ src/main/java/com/github/hyagomv/LearningSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.github.hyagomv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/hyagomv/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.github.hyagomv.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String sqyHello(Model model) {
+		model.addAttribute("msg", "World");
+		return "hello";
+	}
+
+}
